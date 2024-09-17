9a84154e756d528922cfa2950332af30fff4da67
Kelsen Tavares <tiadsrussas@gmail.com>
2024-09-17 15:13:11 -0300

    Atualizacao de usuario com o PUT

--- src/main/java/com/kelsentavares/workshopmongo/resouce/UserResouce.java
+++ src/main/java/com/kelsentavares/workshopmongo/resouce/UserResouce.java
@@ -55,5 +55,14 @@ public class UserResouce {
 			
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+					
+	}
+	
 
 }
